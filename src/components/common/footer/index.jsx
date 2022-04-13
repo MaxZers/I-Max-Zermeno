5c8a696cb930d77db8d02826983a14319aced091
Maximiliano Zermeño <rodrigo.zermenosan@gmail.com>
2022-04-13 12:50:49 -0700

    Footer and heater finished

--- src/components/common/footer/index.jsx
+++ src/components/common/footer/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './index.css';
+import facebookicon from '../../../assets/facebookicon.png';
+import koficon from '../../../assets/koficon.png';
 
 const Footer = () => (
     <>
@@ -7,8 +9,18 @@ const Footer = () => (
             <h3>Contácto</h3>
             <div className="texts">
                 <p><span>Correo: </span> rodrigo.zermenosan@gmail.com</p>
-                <p><span>Facebook: </span> <a href="https://www.facebook.com/IdeasMaxzermeno">Imagen pendiente</a></p>
-                <p><span>Ko-fi: </span> <a href="https://ko-fi.com/m_zermeno#">Imagen pendiente</a></p>
+                <p>
+                    <span>Facebook: </span>
+                    <a href="https://www.facebook.com/IdeasMaxzermeno" target={"_blank"}>
+                        <img src={facebookicon} width={35} height={35} alt="facebook icon"/>
+                    </a>
+                </p>
+                <p>
+                    <span>Ko-fi: </span>
+                    <a href="https://ko-fi.com/m_zermeno#" target={"_blank"}>
+                        <img src={koficon} width={35} height={35} alt="ko-fi icon" />
+                    </a>
+                </p>
             </div>
         </footer>
     </>
---
+++ src/components/homepage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index';
+import { Link } from 'react-router-dom';
+
+const Homepage = () => (
+    <>
+        s
+    </>
+)
